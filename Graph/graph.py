acd8af0f29bb362e422a3acc8f76557006e265ef
Usmaelabdureman <uabdureman@gmail.com>
2022-06-25 00:18:46 +0300

    shortest path

--- Graph/graph.py
+++ Graph/graph.py
@@ -15,3 +15,27 @@ def get_graph(filename):
             elif len(file_content) == 6:
                 adjacent_list[file_content[0]] = [file_content[1], file_content[2], file_content[3],
                                                   file_content[4]]
+    return adjacent_list
+vertex=[]
+adjacent=[]
+edges=[] 
+def bfs(graph, src, dest):
+    queue = deque()
+    visited = {src: 1}
+    parent = {}
+    queue.append(src)
+    while queue:
+        last = queue.popleft()
+        if last == dest:
+            node = last
+            path = []
+            while node:
+                path.append(node)
+                node = parent.get(node, None)
+            return path[::-1]
+        for neighbors in graph[last]:
+            if neighbors[0] not in visited:
+                visited[neighbors[0]] = 1
+                parent[neighbors[0]] = last
+                queue.append(neighbors[0])
+    return "graph is not connected"
