659a9bb6e389766184bab0aa30b993e2b84ec0b5
MuleHakim <mulukenhakim97@gmail.com>
2022-06-25 00:15:36 +0300

    graph

--- Graph/graph.py
+++ Graph/graph.py
@@ -7,3 +7,11 @@ def get_graph(filename):
         for edge in file:
             file_content = re.split('[ \n]', edge)
             if len(file_content) == 3:
+                adjacent_list[file_content[0]] = [file_content[1], file_content[2]]
+            elif len(file_content) == 4:
+                adjacent_list[file_content[0]] = [file_content[1], file_content[2]]
+            elif len(file_content) == 5:
+                adjacent_list[file_content[0]] = [file_content[1], file_content[2], file_content[3]]
+            elif len(file_content) == 6:
+                adjacent_list[file_content[0]] = [file_content[1], file_content[2], file_content[3],
+                                                  file_content[4]]
